397bff695c0d7897a5f1bfcf7a0765e890a1521b
Daan Weustenraad <weustenraad@gmail.com>
2023-05-05 09:41:46 +0200

    improve project breakpoints

--- pages/schrijfsels/[slug].tsx
+++ pages/schrijfsels/[slug].tsx
@@ -3,18 +3,7 @@ import { TArticle } from "../../typings/article";
 import { Db } from "../../stores/Db";
 import { EditButton } from "../../components/EditButton";
 
-export async function getStaticPaths() {
-    const db = new Db();
-    const articles = await db.getArticles();
-    const paths = Object.values(articles).map((a: any) => ({ params: a }));
-
-    return {
-        paths,
-        fallback: false,
-    };
-}
-
-export async function getStaticProps(context: any) {
+export async function getServerSideProps(context: any) {
     const db = new Db();
     const article = await db.getArticle(context.params.slug);
 
@@ -30,7 +19,11 @@ export default function Post({ post }: { post: TArticle }) {
     return (
         <Page header={post.header}>
             <h1>{post.header}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.content }} className="w-full my-4 sm:whitespace-nowrap" style={{ minHeight: "40vh" }} />
+            <div
+                dangerouslySetInnerHTML={{ __html: post.content }}
+                className="w-full my-4 sm:whitespace-nowrap"
+                style={{ minHeight: "40vh" }}
+            />
             <EditButton slug={post.slug} />
         </Page>
     );
